a74d26b26958bce82a0d3d85e6cd0ba3d77735c8
Ідесс Владислав Анатолійович <2111011@KMA.EDU>
2021-10-23 14:43:17 +0300

    opi test

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+	int min, max;
+	float x, h, y;
+	cout << "Enter [min,max] and h" << endl;
+	cin >> min >> max >> h;
+	x = min;
+	cout << "\tX" << "\tY(x)" << endl;
+	while (x <= max + h / 2)
+	{
+		y = (x * x * x) + log(fabs(x)) - 3;
+		cout << setw(7) << x << setw(7) << setprecision(3) << y << endl;
+		x += h;
+	}
+	do
+	{
+		y = (x * x * x) + log(fabs(x)) - 3;
+		cout << setw(7) << x << setw(7) << setprecision(3) << y << endl;
+		x += h;
+	} while (x <= max + h / 2);
+}
